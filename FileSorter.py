9a90b35b69494d5959577d3837cf6a1932ee8870
Darius <48491055+Crambor@users.noreply.github.com>
2020-03-22 10:56:15 +0000

    Uploaded Project Files

---
+++ FileSorter.py
@@ -0,0 +1,84 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+# pip install watchdog for these packages to work
+
+import re
+import os
+import time
+import config
+
+
+class FileHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(config.sorting_dir):
+            if '.' not in filename:
+                continue
+
+            name, extension = filename.rsplit('.', 1)
+            if extension in config.ignored_extensions:
+                continue
+
+            try:
+                folder_dir = f'{config.sorting_dir}\\' + self.get_folder(extension)
+            except TypeError:
+                continue
+            
+            if not os.path.isdir(folder_dir):
+                os.mkdir(folder_dir)
+
+            try:
+                self.move_file(name, extension, folder_dir)
+            except PermissionError:
+                continue
+
+
+    @staticmethod
+    def get_folder(extension):
+        for folder in folders:
+            if extension in folders[folder]:
+                return folder
+
+        if config.misc_enabled:
+            return 'Misc'
+        return None
+
+
+    @staticmethod
+    def move_file(name, extension, folder_dir):
+        def is_file(folder_file):
+            return bool(re.match(fr'{name}.{extension}|{name}(\s\(([0-9]+)\)\.)', folder_file))
+
+        files = sorted(filter(is_file, os.listdir(folder_dir)))
+        files = files[-1:] + files[:-1]
+
+        if files[0:] != [f"{name}.{extension}"]:
+            filename = f"{name}.{extension}"
+        else:
+            i=2
+            for file in files[1:]:
+                num = int(re.search('\s\(([0-9]+)\)\.', file).group(1))
+                if i != num:
+                    break
+
+                i+=1
+
+            filename = f"{name} ({i}).{extension}"
+
+
+        src = f"{config.sorting_dir}\\{name}.{extension}"
+        dst = f"{folder_dir}\\{filename}"
+        os.rename(src, dst)
+
+
+event_handler = FileHandler()
+observer = Observer()
+observer.schedule(event_handler, config.sorting_dir, recursive=False)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
---
+++ config.py
@@ -0,0 +1,70 @@
+import toml
+# pip install toml for this package to work
+
+import os
+from tkinter import Tk
+from tkinter import messagebox
+
+
+config_toml = """# This is the directory to be sorted into the corresponding folders.
+# Input must be a directory e.g C:/Users/Username/Downloads (Please be aware that '\' will not work for the directory)
+sorting_dir = ''
+
+# This will sort any non-ignored file types into a miscellaneous folder.
+# Input must be true/false
+misc_enabled = false
+
+# These file extensions will be ignored.
+ignore = [
+	'ini'
+]
+
+# These are the folders to be sorted into, based on the file extensions input.
+# To create a new folder, follow the format below.
+[folders]
+Images = [
+	'jpg',
+	'jpeg',
+	'png',
+	'gif',
+	'bmp'
+]
+
+Videos = [
+        'webm',
+        'mp4',
+        'avi',
+        'mov',
+        'mkv',
+        'm4v'
+]"""
+
+
+if not os.path.exists('config.txt'):
+    with open('config.txt', 'w') as x:
+        x.write(config_toml)
+        Tk().withdraw()
+        messagebox.showinfo("Config Created", "Config file has been created.\nPlease input your sorting directory into the config and try again.")
+        exit()
+
+try:
+    data = toml.loads(open('config.txt', 'r').read())
+    misc_enabled = data["misc_enabled"]
+    ignored_extensions = data["ignore"]
+    folders = data["folders"]
+    sorting_dir = data["sorting_dir"]
+    if not os.path.exists(sorting_dir):
+        Tk().withdraw()
+        messagebox.showerror("Config Error", "Sorting directory is empty or does not exist.\nPlease check your config file and try again.")
+        exit()
+        
+except toml.decoder.TomlDecodeError:
+    Tk().withdraw()
+    messagebox.showerror("Config Error", "There is likely to be an issue with your sorting directory input.\nPlease check your config file and try again.")
+    exit()
+    
+except KeyError:
+    Tk().withdraw()
+    messagebox.showerror("Config Error", "A required option is missing or renamed.\nPlease check your config file and try again.")
+    exit()
+
